563ce8dc377a31e079bd97fd7207837b3e91beb4
Lonwabo Ramaliwa <Lonwaboramaliwa@gmail.com>
2025-11-02 23:08:51 +0200

    Corrected code & errors and created data.js

---
+++ data.js
@@ -0,0 +1,10 @@
+
+const siteData = [
+  { title: "Home", url: "Home.html", keywords: [] },
+  { title: "Animal Info", url: "Course Types.html", keywords: [] },
+  { title: "Animal Shelter", url: "6 Month Course.html", keywords: [] },
+  { title: "Animal Products", url: "6 Week Courses.html", keywords: [ ] },
+  { title: "Animal Gallery", url: "Home.html", keywords: [] },
+  { title: "Contact Us", url: "Contact Us.html", keywords: [] },
+  { title: "Donate & Volunteer", url: "Cart.html", keywords: [] },
+];
